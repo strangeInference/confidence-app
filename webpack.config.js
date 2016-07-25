6b57a185cd7eb734f0471884a8cbff5b34e5d97f
Nikola Phipps <tesla.phipps@gmail.com>
2016-07-25 15:44:59 -0700

    Fill in webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+//reqire our dependancies
+var path = require('path');
+var webpack = require('webpack');
+var bundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './assets/js/index',
+
+  output: {
+    path: path.resolve('./assets/bundles/'),
+    filename: '[name]-[hash].js',
+  },
+
+  plugins: [
+    new bundleTracker({filename: './webpack-stats.json'}),
+    new webpack.ProvidePlugin({ 
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery' 
+        })
+  ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  }
+
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.jsx']
+  }
+}
